1dbac64032a0dc0e5bbfe20c46b4d2b7573b4da6
Aaron Thon <AaronCThon4815@gmail.com>
2014-05-07 14:40:58 -0500

    If no picture selected it now displays nothing

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,8 +2,8 @@ class Request < ActiveRecord::Base
 	has_one :assignment
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-	# validates :title, presence: true, length: {minimum: 5}
-	# validates_presence_of :user
+	validates :title, presence: true, length: {minimum: 5}
+	validates_presence_of :user
 	
 	has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
