e77debe345c45cc2f5df247b239825980fc8eaf4
Aaron Thon <AaronCThon4815@gmail.com>
2014-05-09 16:57:47 -0500

    Fixed bug (whenever edit user, session ended).

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :requests
   has_many :assignments
   has_many :comments, dependent: :destroy
+  
   validates :password, length: { minimum: 3 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
